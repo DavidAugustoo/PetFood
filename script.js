0134c3cd02c1dd2473cbdf919d44e5ef35b42e32
David Augusto <davidribeiro086@gmail.com>
2022-02-17 15:21:05 -0300

    :sparkles: feat: listagem de produtos no carrinho

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Menu mobile
 
 let menuMobile = document.querySelector('.menu-mobile');
+let menuCartMobile = document.querySelector('.cart-mobile');
 
 menuMobile.addEventListener('click', () => {
     let mobileArea = document.querySelector('.menu-mobile-area');
@@ -15,9 +16,11 @@ menuMobile.addEventListener('click', () => {
 
 });
 
-// Produtos
+// Listagem de Produtos
 
+let cart = [];
 let modalQt = 1;
+let modalKey = 0;
 
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
@@ -30,11 +33,16 @@ productsJson.map((item, index) => {
     productItem.querySelector('.products-card--brand').innerHTML = item.brand;
     productItem.querySelector('.products-card--name').innerHTML = item.name;
     productItem.querySelector('.products-card--price').innerHTML = `R$ ${item.price}`;
+
+    // Listagem de produtos no modal
+
     productItem.addEventListener('click', (e) => {
         e.preventDefault();
+        c('.cart-container').classList.add('closed');
         let modalQt = 1;
 
         let key = e.target.closest('.products-card').getAttribute('data-key');
+        modalKey = key;
         
         c('.products-modal--image img').src = productsJson[key].img;
         c('.products-modal--info h1').innerHTML = productsJson[key].name;
@@ -95,3 +103,119 @@ function closeModal() {
     }, 500);
     
 }
+
+// Carrinho
+
+let cartMenu = c('.menu--item i').addEventListener('click', () => {
+
+    let cartArea = c('.cart-container');
+
+    if(cartArea.classList.contains('closed')) {
+        cartArea.classList.remove('closed');
+    } else {
+        cartArea.classList.add('closed');
+    }
+    
+});
+
+let cartMenuMobile = c('.cart-mobile').addEventListener('click', () => {
+
+    let cartArea = c('.cart-container');
+
+    if(cartArea.classList.contains('closed')) {
+        cartArea.classList.remove('closed');
+    } else {
+        cartArea.classList.add('closed');
+    }
+    
+});
+
+
+c('.products-modal--button-add').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let size = parseInt(c('.products-modal--size.selected').getAttribute('data-key'));
+    let identifier = productsJson[modalKey].id+'@'+size;
+
+    let key = cart.findIndex((item) => {
+        return item.identifier == identifier;
+    });
+
+    if (key > -1) {
+        cart[key].qt += modalKey;
+    } else {
+        cart.push({
+            identifier,
+            id: productsJson[modalKey].id,
+            size,
+            qt: modalQt,
+        });
+
+        modalQt = 1;
+    };
+
+    updateCart();
+    closeModal();
+ 
+});
+
+function updateCart() {
+
+    if(cart.length > 0) {
+        c('.cart-container').classList.remove('closed');
+        c('.cart-product-area').innerHTML = '';
+
+        let total = 0;
+        let frete = 99;
+
+        for(let i in cart){
+            let productItem = productsJson.find((item) => {
+                return item.id == cart[i].id;
+            });
+
+            total += productItem.price * cart[i].qt;
+
+            cartItem = c('.cart-modal .cart-product').cloneNode(true);
+
+            cartItem.querySelector('img').src = productItem.img;
+            cartItem.querySelector('.products-cart--brand').innerHTML = productItem.brand;
+            cartItem.querySelector('.products-cart--name').innerHTML = productItem.name;
+            cartItem.querySelector('.products-cart--price').innerHTML = `R$ ${productItem.price}`;
+            cartItem.querySelector('.cart--qt').innerHTML = cart[i].qt;
+            cartItem.querySelector('.cart--qtmenos').addEventListener('click', () => {
+                if(cart[i].qt > 1) {
+                    cart[i].qt--;
+                } else {
+                    cart.splice(i, 1);
+                }
+                updateCart();
+            });
+            cartItem.querySelector('.cart--qtmais').addEventListener('click', () => {
+                cart[i].qt++;    
+                updateCart();
+            });
+
+            c('.cart-product-area').append(cartItem);
+
+        }
+        
+        frete = frete - total;
+
+        c('.cart-info--price').innerHTML = `R$ ${total.toFixed(2)}`;
+
+        if(total < 99) {
+            c('.cart-info--frete').innerHTML = `Faltam R$ ${frete.toFixed(2)} para receber frete grátis`;
+        } else {
+            c('.cart-info--frete').innerHTML = `Parabéns! Frete grátis aplicado`;
+        }
+        
+        
+
+    } else {
+        c('.cart-container').classList.add('closed');
+        c('.cart-product-area').innerHTML = '';
+        c('.cart-info--price').innerHTML = `R$ 0`;
+        c('.cart-info--frete').innerHTML = ``;
+    }
+}
+
